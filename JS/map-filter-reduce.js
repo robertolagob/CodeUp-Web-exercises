0619e7f647042ea15cd68dc517fc6a3894a2bce6
robertolago <robertolagob@gmail.com>
2018-04-19 23:29:06 -0500

    chore: ecmascript6 exercises

---
+++ JS/map-filter-reduce.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+
+    let POLIGLOT= users
+    .filter(users=>users.languages.length>2);
+     console.log(POLIGLOT);
+
+
+
+    let EMAILS=users.map(users=>users.email);
+     console.log(EMAILS);
+
+
+
